b648724d0ddcbfc9daf9a9975452787b5f32e817
Devang Gajjar <DevangGajjar077@gmail.com>
2026-03-02 19:39:20 +0530

    query chages

--- app/Providers/RolesPermissionServiceProvider.php
+++ app/Providers/RolesPermissionServiceProvider.php
@@ -23,7 +23,7 @@ class RolesPermissionServiceProvider extends ServiceProvider
     public function boot(): void
     {
         try {
-            Permission::get()->map(function ($permission) {
+            Permission::query()->each(function ($permission) {
                 Gate::define($permission->slug, function ($user) use ($permission) {
                     return $user->hasPermissionTo($permission);
                 });
